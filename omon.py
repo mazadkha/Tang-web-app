7950a74c82ec5b05f7bde9b28f0c2d0b3d9bce2e
Mak <azadkha@gmail.com>
2022-11-06 23:02:07 -0500

    Added user fetch and added top bar

--- omon.py
+++ omon.py
@@ -13,7 +13,8 @@ app = Flask(__name__)  # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
+    return render_template('index.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
